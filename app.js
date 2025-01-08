3dc18e8038777983101e83cab3c80cd382275625
ZeinaZayed4 <zeinazayed@std.mans.edu.eg>
2025-01-08 11:57:03 +0200

    Fetching data

--- app.js
+++ app.js
@@ -17,5 +17,15 @@ connectToDb((err) => {
 
 // routes
 app.get("/books", (req, res) => {
-	res.json({ mssg: "Welocme to the api" });
+	let books = [];
+	db.collection("books")
+		.find()
+		.sort({ author: 1 })
+		.forEach((book) => books.push(book))
+		.then(() => {
+			res.status(200).json(books);
+		})
+		.catch(() => {
+			res.status(500).json({ error: "Could not fetch the documents." });
+		});
 });
